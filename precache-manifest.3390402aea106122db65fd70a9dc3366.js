1b3ac8ef29d47ec70811f8071c74e2b6fdbb492e
yousef2213 <yousef2213>
2021-07-22 13:25:36 +0200

    Updates

---
+++ precache-manifest.3390402aea106122db65fd70a9dc3366.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b850dd2a9d3ebaa2883c72392d356095",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "fa71a4be98cc9a741179",
+    "url": "/react-app/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "5d0c0fde48504f44a01e",
+    "url": "/react-app/static/css/main.23af83f4.chunk.css"
+  },
+  {
+    "revision": "fa71a4be98cc9a741179",
+    "url": "/react-app/static/js/2.ec5688b5.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react-app/static/js/2.ec5688b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d0c0fde48504f44a01e",
+    "url": "/react-app/static/js/main.1ff6918c.chunk.js"
+  },
+  {
+    "revision": "0e041c89f227aafa7eb6",
+    "url": "/react-app/static/js/runtime-main.16d19cca.js"
+  },
+  {
+    "revision": "420ca8f3d197fc82ca8681e753415f43",
+    "url": "/react-app/static/media/banner.420ca8f3.jpeg"
+  },
+  {
+    "revision": "1b18881aaca70ccbc0a9a502a9d86900",
+    "url": "/react-app/static/media/single.1b18881a.jpeg"
+  }
+]);
